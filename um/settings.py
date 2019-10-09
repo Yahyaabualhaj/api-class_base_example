3601d4040305811461ea5cc76bb0dc20bd79da46
yahyaabualhaj <yahya.abualhaj@gmail.com>
2019-10-09 08:59:51 +0000

    settings update

--- um/settings.py
+++ um/settings.py
@@ -15,6 +15,12 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+try:
+    from .local_settings import *
+except ImportError:
+    pass
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
