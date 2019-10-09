bb4e461850e584f7c6f0e03d6f3bcfcf82b430b8
yahyaabualhaj <yahya.abualhaj@gmail.com>
2019-10-09 12:50:44 +0300

    add static settings

--- um/settings.py
+++ um/settings.py
@@ -103,7 +103,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT= os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'btre/static')
+]
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
