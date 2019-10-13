c2470525c09f17217090a6f1440e236b0151f991
yahyaabualhaj <yahya.abualhaj@gmail.com>
2019-10-13 10:42:46 +0300

    r3

--- um/local_settings.py
+++ um/local_settings.py
@@ -12,7 +12,7 @@ ALLOWED_HOSTS = ['178.62.251.45']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'api_um_db',
+        'NAME': 'um_prod',
         'USER': 'dbadmin',
         'PASSWORD': 'asdasdasd',
         'HOST': 'localhost',
